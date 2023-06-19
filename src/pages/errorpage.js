9bdb42bb42eebbb2fe2392426ffb7b7904c52473
Felipe Smith <69473215+smith1813@users.noreply.github.com>
2023-06-19 17:05:12 -0400

    css changes for error page

--- src/pages/errorpage.js
+++ src/pages/errorpage.js
@@ -5,11 +5,24 @@ import { Link } from "react-router-dom";
 export default function ErrorPage() {
 
   return (
-    <div id="error-page" className="bg-slate-800 h-screen w-screen m-0 p-0 flex justify-center align-middle">
-        <div className=" relative pt-auto text-center m-auto text-3xl font-bold flex justify-center ">
-            <img className="w-100% h-auto" src={require('../images/error404.jpg')} />
-            <div className="truncate absolute m-0 p-5 border-4 bg-opacity-60 bg-slate-900  text-white border-slate-100 top-2/3 left-1/2  -translate-x-1/2 -translate-y-1/2">
-                <h1 className="m-0 text-4xl">Oops!</h1>
+    <div id="error-page" className="
+        bg-slate-800 
+        h-screen w-screen 
+        overflow-auto 
+        m-0 p-0 
+        flex justify-center ">
+        
+        <div name="error_container" className="relative h-2/3 pt-auto m-auto flex justify-center object-scale-down h-auto ">
+            
+            <img name="error_image_container" 
+                className="" src={require('../images/error404.jpg')} />
+            
+            <div name="error_mesage_container" 
+                className="truncate text-center font-bold absolute m-0 p-5 border-4 
+                bg-opacity-60 bg-slate-900  text-white border-slate-100 
+                top-2/3 left-1/2  -translate-x-1/2 -translate-y-1/2 ">
+                
+                <div className="m-0 mb-1 text-4xl">Oops!</div>
                 <div className="m-0 text-xl">
                     Sorry, an unexpected error has occurred.<br/>
                     Maybe you put the wrong URL?
